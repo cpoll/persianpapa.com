7e740d4878ce0d252accfa865a2d59350a895378
Cristian Poll <cristian@cristianpoll.com>
2018-10-30 19:16:04 -0400

    add threshhold for visits

--- site/main.js
+++ site/main.js
@@ -1,27 +1,41 @@
 (() => {
+
+    function visitMeetsThreshhold(lastVisit) {
+        const THRESHHOLD_SECONDS = 60 * 5;
+
+        if(!lastVisit) {
+            return False;
+        }
+
+        return Date.now() - parseInt(lastVisit) > 1000 * THRESHHOLD_SECONDS;
+    }
+
     function updateStats(){
         const TIMES_VISITED_KEY = 'timesVisited';
-        const FIRST_VISIT_KEY = 'fistVisit';
-        const LAST_VISIT_KEY = 'lastVisit';
+        const FIRST_VISIT_KEY = 'firstVisit';
+        const LATEST_VISIT_KEY = 'lastVisit';
 
         let timesVisited = Cookies.get(TIMES_VISITED_KEY);
-        if(timesVisited) {
-            timesVisited++;
+        const firstVisit = Cookies.get(FIRST_VISIT_KEY);
+        const latestVisit = Cookies.get(LATEST_VISIT_KEY);
+
+        if(timesVisited && firstVisit) {
+            if(visitMeetsThreshhold(latestVisit)) {
+                Cookies.set(TIMES_VISITED_KEY, ++timesVisited);
+                Cookies.set(LATEST_VISIT_KEY, Date.now());
+            }
 
             const statsElement = document.getElementsByClassName('stats')[0];
 
-            const lastVisit = Cookies.get(FIRST_VISIT_KEY);
-            formattedDate = new Date(lastVisit).toLocaleDateString("en-CA", {month: 'long', year: "numeric", day: "numeric"});
+            formattedDate = new Date(parseInt(firstVisit)).toLocaleDateString("en-CA", {month: 'long', year: "numeric", day: "numeric"});
 
             statsElement.innerHTML = `Your browser has gone wandering ${timesVisited} times since ${formattedDate}`;
         }
         else {
-            timesVisited = 1;
+            Cookies.set(TIMES_VISITED_KEY, 1);
             Cookies.set(FIRST_VISIT_KEY, Date.now());
+            Cookies.set(LATEST_VISIT_KEY, Date.now());
         }
-
-        Cookies.set(LAST_VISIT_KEY, Date.now());
-        Cookies.set(TIMES_VISITED_KEY, timesVisited);
     }
 
     document.addEventListener("DOMContentLoaded", updateStats);
